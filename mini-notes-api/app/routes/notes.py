ebf84ef79871918c321d0570777237dbbf024609
eigenlambda123 <rmvilla987@gmail.com>
2025-06-18 10:54:24 +0800

    get_notes endpoint

--- mini-notes-api/app/routes/notes.py
+++ mini-notes-api/app/routes/notes.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from app.schemas import Note
+from app.models import notes_db
 
 router = APIRouter()
 
-@router.get("/notes")
+@router.get("/notes", response_model=list[Note]) # endpoint to retrieve all notes
 def get_notes():
-    return {"notes": []}
+    """
+    Retrieve all notes
+    """
+    return notes_db
