1f7a79d5edddba9a5b6676557e79d9d046213f0c
eigenlambda123 <rmvilla987@gmail.com>
2025-06-18 11:02:02 +0800

    read_note by id endpoint

--- mini-notes-api/app/routes/notes.py
+++ mini-notes-api/app/routes/notes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.schemas import Note
 from app.models import notes_db
+from uuid import UUID
 
 router = APIRouter()
 
@@ -20,3 +21,14 @@ def create_note(note: Note):
     new_note = Note(**note.dict()) # auto generate id and created_at
     notes_db.append(new_note) # add the new note to the in-memory database
     return new_note
+
+
+@router.get("/notes/{note_id}", response_model=Note) # endpoint to retrieve a note by id
+def read_note(note_id: UUID):
+    """
+    Retrieve a note by ID
+    """
+    for note in notes_db:
+        if note.id == note_id:
+            return note
+    raise HTTPException(status_code=404, detail="Note not found") # raise an error if the note is not found
