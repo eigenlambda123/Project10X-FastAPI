dded931ee04bf264b020df7d55ba8fc0988d6693
eigenlambda123 <rmvilla987@gmail.com>
2025-06-18 11:46:54 +0800

    update_note endpoint

--- mini-notes-api/app/routes/notes.py
+++ mini-notes-api/app/routes/notes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from app.schemas import Note
+from app.schemas import Note, NoteUpdate
 from app.models import notes_db
 from uuid import UUID
 
@@ -31,4 +31,26 @@ def read_note(note_id: UUID):
     for note in notes_db:
         if note.id == note_id:
             return note
+    raise HTTPException(status_code=404, detail="Note not found")
+
+
+
+@router.put("/notes/{note_id}", response_model=Note) # endpoint to update a note by id
+def update_note(note_id: UUID, updated_note: NoteUpdate):
+    """
+    Update a note by ID
+    """
+    for index, existing_note in enumerate(notes_db): #  find the note to update
+
+        if existing_note.id == note_id: # if the note is found
+            note_data = existing_note.model_dump() #  Get the existing note data as a dictionary
+            update_data = updated_note.model_dump(exclude_unset=True) # Get the updated data, excluding unset fields
+            
+
+            note_data.update(update_data) # update the existing note data with the new values
+            for key, value in update_data.items():
+                setattr(existing_note, key, value)  # update fields in place
+
+            return existing_note # return the updated note
+        
     raise HTTPException(status_code=404, detail="Note not found") # raise an error if the note is not found
