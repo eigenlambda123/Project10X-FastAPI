986202fc431884674f083e24b12678e6e934c0a0
eigenlambda123 <rmvilla987@gmail.com>
2025-06-21 11:33:54 +0800

    created BookCreate and BookUpdate model that inherit from BookBase model

--- book-review-api/app/schemas.py
+++ book-review-api/app/schemas.py
@@ -9,7 +9,16 @@ class BookBase(BaseModel):
     author: str = Field(..., max_length=100) # Author of the book
     genre: Optional[str] = Field(None, max_length=50) # Genre of the book, optional
     published_year: int = Field(..., ge=0) # Year the book was published, must be a positive integer
+    
 
+class BookCreate(BookBase):
+    """Schema for creating a new book that inherits from BookBase"""
+    pass
+
+
+class BookUpdate(BookBase):
+    """Schema for updating an existing book that inherits from BookBase"""
+    pass
 
 
 
