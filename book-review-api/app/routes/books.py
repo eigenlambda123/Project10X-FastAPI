fa3b8042fd186fe5eedbd74b8498a47c163bfb14
eigenlambda123 <rmvilla987@gmail.com>
2025-06-21 12:17:33 +0800

    Add POST endpoint for creating new books

--- book-review-api/app/routes/books.py
+++ book-review-api/app/routes/books.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.models import books_db, find_book_by_id, find_reviews_by_book_id
 from app.schemas import Book 
 from fastapi import HTTPException
+from uuid import uuid4
 
 router = APIRouter()
 
@@ -24,3 +25,16 @@ def get_book(book_id: str):
     # Include nested reviews
     book["reviews"] = find_reviews_by_book_id(book_id)
     return book
+
+
+
+
+
+@router.post("/", response_model=Book, status_code=201)
+def create_book(book: Book):
+    """Create new book"""
+    
+    new_book = book.model_dump(exclude_unset=True) # Convert Pydantic model to dict, excluding unset fields
+    new_book["id"] = str(uuid4()) # Generate a new UUID for the book ID
+    books_db.append(new_book) # Add the new book to the in-memory database
+    return new_book # Return the newly created book with its ID
