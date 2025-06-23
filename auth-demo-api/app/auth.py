ccbca87f155a4749b3371c4ddc8064e5e6ec060c
eigenlambda123 <rmvilla987@gmail.com>
2025-06-23 12:25:40 +0800

    added a utility function that will get user by their username

--- auth-demo-api/app/auth.py
+++ auth-demo-api/app/auth.py
@@ -1,5 +1,6 @@
 
 from passlib.context import CryptContext
+from .database import fake_users_db
 
 SECRET_KEY = "secret"
 ALGORITHM = "HS256"
@@ -12,3 +13,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a plain password against a hashed password."""
     return pwd_context.verify(plain_password, hashed_password)
+
+def get_user_by_username(username: str) -> dict | None:
+    """Get a user by their username"""
+    return fake_users_db.get(username)
