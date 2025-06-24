833a0ba0a8b7eab860707fbe3d3de2f904560aa7
eigenlambda123 <rmvilla987@gmail.com>
2025-06-24 15:40:44 +0800

    added GET endpoint for getting the currently authenticated admin

--- auth-demo-api/app/routes/users.py
+++ auth-demo-api/app/routes/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.deps import get_current_user
+from app.deps import get_current_user, get_current_admin
 from app.schemas import UserOut
 from uuid import UUID
 
@@ -10,3 +10,8 @@ router = APIRouter()
 async def read_current_user(current_user: dict = Depends(get_current_user)):
     """Get the current logged-in user"""
     return current_user
+
+@router.get("/admin", response_model=UserOut)
+async def read_admin_data(current_admin: dict = Depends(get_current_admin)):
+    """Get data accessible only by admin users"""
+    return current_admin
