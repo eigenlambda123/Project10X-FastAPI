778e5314bf9a3f76221af41239f2066127160ab9
eigenlambda123 <rmvilla987@gmail.com>
2025-06-24 14:56:12 +0800

    add get_current_user for decoding the JWT token and getting the users information

---
+++ auth-demo-api/app/deps.py
@@ -0,0 +1,27 @@
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from app.schemas import TokenData
+from app.database import fake_users_db
+from app.auth import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    """
+    Get the current user from the JWT token
+    Decodes the JWT token and retrieves the user information
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # decode the token
+        username: str = payload.get("sub") # get the username from the token payload
+        role: str = payload.get("role") # get the role from the token payload
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = fake_users_db.get(username)
+        if user is None:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
