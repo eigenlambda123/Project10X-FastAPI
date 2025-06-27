1b6c197b0704d891772bd125c0a5d51724bd0866
eigenlambda123 <rmvilla987@gmail.com>
2025-06-27 10:39:14 +0800

    created Schema for Report Request use for validation

--- background-tasks-service/app/routes/reports.py
+++ background-tasks-service/app/routes/reports.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, BackgroundTasks
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class ReportRequest(BaseModel):
+    """Schema for Report Request use for validation"""
+    name: str
+    details: dict
 
 @router.post("/")
 def generate_report(background_tasks: BackgroundTasks):
