f78ded45471608fed0e20e6eb06f66912f37990f
eigenlambda123 <rmvilla987@gmail.com>
2025-06-30 10:03:53 +0800

    get_from_cache to Retrieve data from cache by key

--- weather-proxy-service/app/utils/cache.py
+++ weather-proxy-service/app/utils/cache.py
@@ -15,3 +15,23 @@ def make_cache_key(city: str = "", lat: float = None, lon: float = None) -> str:
     elif lat is not None and lon is not None:
         return f"coords:{lat:.4f},{lon:.4f}"
     return "invalid"
+
+
+def get_from_cache(key: str):
+    """Retrieve data from cache by key"""
+    # get the entry from the cache
+    entry = cache_store.get(key) 
+
+    # if the entry does not exist or has expired, return None
+    if not entry:
+        return None
+    
+    # check if the entry has expired
+    expires_at, data = entry
+
+    # If the current time is greater than the expiration time, remove it and return None
+    if time.time() > expires_at:
+        # remove the expired entry from the cache
+        del cache_store[key]  
+        return None
+    return data
