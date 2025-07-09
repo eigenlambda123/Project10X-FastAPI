d2c68df5967f2212c99482691a4c0ef1413984c3
eigenlambda123 <rmvilla987@gmail.com>
2025-07-09 12:13:59 +0800

    POST endpoint to create a new blog post

--- async-blog-api/app/routers/posts.py
+++ async-blog-api/app/routers/posts.py
@@ -1,5 +1,12 @@
+from fastapi import Depends, APIRouter
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.db import async_session
+
+from app.models import BlogPost, Tag, PostTagLink
+from app.schemas import BlogPostCreate, BlogPostRead
+
+router = APIRouter(prefix="/posts", tags=["Posts"])
+
 
 async def get_session() -> AsyncSession:
     """
@@ -7,3 +14,16 @@ async def get_session() -> AsyncSession:
     """
     async with async_session() as session:
         yield session
+
+
+@router.post("/", response_model=BlogPostRead)
+async def create_post(post: BlogPostCreate, session: AsyncSession = Depends(get_session)):
+    """
+    POST endpoint to create a new blog post
+    """
+
+    db_post = BlogPost(title=post.title, content=post.content)
+    session.add(db_post)
+    await session.commit()
+    await session.refresh(db_post)
+    return db_post
