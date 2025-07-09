f40e813d1056dff92616b44b97e69f072f0c702d
eigenlambda123 <rmvilla987@gmail.com>
2025-07-09 12:21:36 +0800

    Updated POST endpoint to create a new blog post

--- async-blog-api/app/main.py
+++ async-blog-api/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.db import create_db_and_tables
+from app.routers import posts
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup():
     await create_db_and_tables()
-    
+
+app.include_router(posts.router) 
--- async-blog-api/app/routers/posts.py
+++ async-blog-api/app/routers/posts.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, APIRouter
-from sqlmodel.ext.asyncio.session import AsyncSession
 from app.db import async_session
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import BlogPost, Tag, PostTagLink
+from app.models import BlogPost, Tag
 from app.schemas import BlogPostCreate, BlogPostRead
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
@@ -18,12 +19,20 @@ async def get_session() -> AsyncSession:
 
 @router.post("/", response_model=BlogPostRead)
 async def create_post(post: BlogPostCreate, session: AsyncSession = Depends(get_session)):
-    """
+    """ 
     POST endpoint to create a new blog post
+    Args:
+        post (BlogPostCreate): The blog post data to create
+        session (AsyncSession): The database session
     """
-
     db_post = BlogPost(title=post.title, content=post.content)
+
+    # if there are tags, attach them via the relationship
+    if post.tag_ids:
+        tags = await session.exec(select(Tag).where(Tag.id.in_(post.tag_ids)))
+        db_post.tags = tags.all()
+    
     session.add(db_post)
     await session.commit()
-    await session.refresh(db_post)
+    await session.refresh(db_post, attribute_names=["tags"])
     return db_post
