d58415867bdf9720332343c127d0a8206e92d289
eigenlambda123 <rmvilla987@gmail.com>
2025-07-10 15:41:16 +0800

    GET endpoint to read all tags

--- async-blog-api/app/routers/tags.py
+++ async-blog-api/app/routers/tags.py
@@ -26,3 +26,12 @@ async def create_tag(tag: TagCreate, session: AsyncSession = Depends(get_session
     await session.commit()
     await session.refresh(db_tag)
     return db_tag
+
+
+@router.get("/", response_model=List[TagRead])
+async def read_tags(session: AsyncSession = Depends(get_session)):
+    """
+    GET endpoint to read all tags
+    """
+    result = await session.exec(select(Tag))
+    return result.all()
