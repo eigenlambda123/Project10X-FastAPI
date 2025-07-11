f30baf6c5ac65474ba19b65a64d80d5e77f7a559
eigenlambda123 <rmvilla987@gmail.com>
2025-07-11 09:40:52 +0800

    get_session() setup

--- async-blog-api/app/routers/comments.py
+++ async-blog-api/app/routers/comments.py
@@ -1,3 +1,12 @@
 from fastapi import APIRouter
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.db import async_session
 
 router = APIRouter(prefix="/posts/{post_id}/comments", tags=["Comments"])
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        """Get an async session for database operations"""
+        yield session
+
