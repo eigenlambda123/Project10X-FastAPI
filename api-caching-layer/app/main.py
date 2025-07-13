b2f942771ddc254eb01a4d0d146af05bb39a6096
eigenlambda123 <rmvilla987@gmail.com>
2025-07-13 11:14:45 +0800

    GET user details and cache the response

--- api-caching-layer/app/main.py
+++ api-caching-layer/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routers import test
+from app.routers import test, users
 
 app = FastAPI()
 
 
 app.include_router(test.router)
+app.include_router(users.router, prefix="/api", tags=["users"])
---
+++ api-caching-layer/app/routers/users.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Depends, Request
+from app.cache.redis_client import get_redis_client
+from app.cache.decorator import cache_response
+
+router = APIRouter()
+
+@router.get("/users/{user_id}")
+@cache_response(ttl=120) # use custom cache decorator with a TTL of 120 seconds
+async def get_user(user_id: int, request: Request, redis = Depends(get_redis_client)):
+    return {"user_id": user_id, "name": "Cached User"}
