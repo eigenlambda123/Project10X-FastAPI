dfb60c6be9b08e73093d0c4eadd696efada0e73d
eigenlambda123 <rmvilla987@gmail.com>
2025-07-13 11:28:53 +0800

    register cache_admin

--- api-caching-layer/app/main.py
+++ api-caching-layer/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import test, users, posts
+from app.routers import test, users, posts, cache_admin
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app = FastAPI()
 app.include_router(test.router)
 app.include_router(users.router, prefix="/api", tags=["users"])
 app.include_router(posts.router, prefix="/api", tags=["posts"])
+app.include_router(cache_admin.router, prefix="/api/admin", tags=["cache_admin"])
