aa3d68971dbe942d38a61b93512a83f0785c1a4d
eigenlambda123 <rmvilla987@gmail.com>
2025-07-13 12:26:11 +0800

    include metrics

--- api-caching-layer/app/main.py
+++ api-caching-layer/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import test, users, posts, cache_admin
+from app.routers import test, users, posts, cache_admin, metrics
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ app.include_router(test.router)
 app.include_router(users.router, prefix="/api", tags=["users"])
 app.include_router(posts.router, prefix="/api", tags=["posts"])
 app.include_router(cache_admin.router, prefix="/api/admin", tags=["cache_admin"])
+app.include_router(metrics.router, prefix="/api/metrics", tags=["metrics"])
