e0167fa6b29685ed9199fc79296d574984bd8914
eigenlambda123 <rmvilla987@gmail.com>
2025-07-13 12:18:20 +0800

    Implemented cache metrics

---
+++ api-caching-layer/app/cache/metrics.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+cache_stats = {
+    "hits": 0,
+    "misses": 0,
+    "access_log": Counter()  # Tracks key access frequency
+}
+
+def record_hit(key: str):
+    cache_stats["hits"] += 1
+    cache_stats["access_log"][key] += 1
+
+def record_miss(key: str):
+    cache_stats["misses"] += 1
+    cache_stats["access_log"][key] += 1
+
+def get_metrics():
+    top_keys = cache_stats["access_log"].most_common(5)
+    return {
+        "hits": cache_stats["hits"],
+        "misses": cache_stats["misses"],
+        "top_keys": top_keys,
+        "total_cached_keys": None  # Optional, to be filled from Redis
+    }
