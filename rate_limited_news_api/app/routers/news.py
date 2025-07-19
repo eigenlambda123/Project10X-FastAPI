e9900ea5c61cf4f75d27b3a8d105fd77e00241b4
eigenlambda123 <rmvilla987@gmail.com>
2025-07-19 10:58:01 +0800

    remove import

--- rate_limited_news_api/app/routers/news.py
+++ rate_limited_news_api/app/routers/news.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Request
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 from app.rate_limit import limiter
-from app.scraper import mock_scrape_news
 from app.scraper import get_all_news
 
 router = APIRouter(prefix="/news")
