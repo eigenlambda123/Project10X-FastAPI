a263c6648161ea466a34aa87807ab2b464e117c8
eigenlambda123 <rmvilla987@gmail.com>
2025-07-20 09:33:11 +0800

    custom rate_limit handler

--- rate_limited_news_api/app/rate_limit.py
+++ rate_limited_news_api/app/rate_limit.py
@@ -1,4 +1,15 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from fastapi import Request
+from fastapi.responses import JSONResponse
 
 limiter = Limiter(key_func=get_remote_address)
+
+
+# Custom handler
+async def rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Rate limit exceeded. Too many requests from this IP. Try again later."},
+    )
