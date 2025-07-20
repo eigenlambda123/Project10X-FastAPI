f36cb7577acddaa0b9f24e35e3628b49fb227811
eigenlambda123 <rmvilla987@gmail.com>
2025-07-20 10:57:49 +0800

    added multiple user agents

--- rate_limited_news_api/app/scraper.py
+++ rate_limited_news_api/app/scraper.py
@@ -6,9 +6,13 @@ from app.redis_cache import get_cache, set_cache
 from app.logger import logger
 import time
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/109.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 Version/15.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+]
 
 
 async def fetch_html(url: str) -> str | None:
