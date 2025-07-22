92222c275e836219ad2173ed864e630da603c9fe
eigenlambda123 <rmvilla987@gmail.com>
2025-07-22 09:09:49 +0800

    refactor db setup, added get_session for dependency injection

--- dockerized-production-api/app/db.py
+++ dockerized-production-api/app/db.py
@@ -4,9 +4,25 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.config import settings
 
 # Create an async SQLAlchemy engine
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True,
+    )
 
-async def init_db():
+# Create an async session factory
+async_session_factory = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+# dependency to get a session
+async def get_session() -> AsyncSession:
+    async with async_session_factory() as session:
+        yield session
+
+
+# For table creation at startup
+async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
