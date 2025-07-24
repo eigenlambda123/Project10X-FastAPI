ffa08ca3c1f980ad5eb726ab65a6aa45cd54e231
eigenlambda123 <rmvilla987@gmail.com>
2025-07-24 10:25:22 +0800

    fix env error

--- dockerized-production-api/app/core/config.py
+++ dockerized-production-api/app/core/config.py
@@ -5,8 +5,10 @@ class Settings(BaseSettings):
     Application settings loaded from environment variables
     """
     DATABASE_URL: str
+    ENV: str = "development"
 
     class Config:
         env_file = ".env"
+        extra = "allow"
 
 settings = Settings()
