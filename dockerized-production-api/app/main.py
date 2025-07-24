849d47e81b6f9439cbd6faf8c1c399a5ca6b8a34
eigenlambda123 <rmvilla987@gmail.com>
2025-07-24 10:33:13 +0800

    initialize logging and middleware in main

--- dockerized-production-api/app/main.py
+++ dockerized-production-api/app/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from app.db import create_db_and_tables
 from app.core.config import settings
 from app.routers import items, metrics
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.logging_config import setup_logging
 
+
+setup_logging()
 app = FastAPI(title="Dockerized Production API")
+app.add_middleware(LoggingMiddleware)
+
 
 @app.on_event("startup")
 async def on_startup():
